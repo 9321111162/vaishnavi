f9b93e2d5f6f209aa5ba95d640d5147301e14c48
9321111162 <114163829+9321111162@users.noreply.github.com>
2022-10-19 15:03:40 +0530

    Add files via upload

---
+++ day 5/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './forms in react/App';
+//import App from './time exercise/App';
+
+
+ReactDOM.render(
+   <App></App>,
+   document.getElementById('root')
+);
